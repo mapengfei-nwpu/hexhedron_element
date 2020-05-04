95839e83384f5e516b0ef94cc22cd5f9a8183bbc
Ma Pengfei <merrily@live.com>
2020-05-04 15:45:59 +0800

    a

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+
+
 #include <dolfin.h>
+#include "TetPoisson.h"
 #include "HexPoisson.h"
 #include "IBMesh.h"
+#include "IBInterpolation.h"
 #include "LocalPolynomial.h"
-#include <random>
 
 using namespace dolfin;
 
@@ -12,46 +15,57 @@ public:
 	xyz() : Expression(3) {}
 	void eval(Array<double> &values, const Array<double> &x) const
 	{
-		values[0] = x[0] * x[0];
-		values[1] = x[1] * x[1];
-		values[2] = x[2] * x[2];
+		values[0] = x[0];
+		values[1] = x[1];
+        values[2] = x[2];
 	}
 };
 
 int main()
 {
-	size_t nx = 10;
-
-	std::random_device rd;
-	std::uniform_real_distribution<double> uu(0, 1);
-
+	size_t nx;
+	std::cin >> nx;
 	auto g = std::make_shared<xyz>();
 
 	Point p0(0, 0, 0);
 	Point p1(1, 1, 1);
 	IBMesh ib_mesh({p0, p1}, {nx, nx, nx});
-
-	auto mesh = ib_mesh.mesh_ptr();
 	auto U = std::make_shared<HexPoisson::FunctionSpace>(ib_mesh.mesh_ptr());
 	Function u(U);
 	u.interpolate(*g);
 
-	LocalPolynomial local_poly;
-	local_poly.function = u;
-	while (true)
+    auto sphere = std::make_shared<Mesh>("./sphere.xml.gz");
+	auto V = std::make_shared<TetPoisson::FunctionSpace>(sphere);
+	Function v(V);
+    
+    std::cout<<"interpolation"<<std::endl;
+	DeltaInterplation di(ib_mesh);
+	di.fluid_to_solid(u,v);
+
+	File file("xyz.pvd");
+	file << v;
+
+/*
+auto sum = 0.0;
+	auto aa = v.function_space()->tabulate_dof_coordinates();
+	for (size_t i = 0; i < aa.size()/9; i++)
 	{
-		Point p2(uu(rd), uu(rd), uu(rd));
-		auto global_index = ib_mesh.hash(p2);
-		auto local_index = ib_mesh.global_map[global_index][1];
-
-		auto values = local_poly.eval(p2, local_index);
-
-		std::cout
-			<< sqrt(values[0]) << ","
-			<< sqrt(values[1]) << ","
-			<< sqrt(values[2]) << ","
-			<< std::endl;
-
-		std::cout << p2 << std::endl;
+		std::cout<<"error1: "
+		         << aa[i*9] -v.vector()->getitem(i*3) << std::endl;
+	sum+=abs(aa[i*9] -v.vector()->getitem(i*3));
 	}
+	for (size_t i = 0; i < aa.size()/9; i++)
+	{
+		std::cout<<"error2: "
+		         << aa[i*9+1] -v.vector()->getitem(i*3+1) << std::endl;
+	sum+=abs(aa[i*9+1] -v.vector()->getitem(i*3+1));
+	}
+	for (size_t i = 0; i < aa.size()/9; i++)
+	{
+		std::cout<<"error3: "
+		         << aa[i*9+2] -v.vector()->getitem(i*3+2) << std::endl;
+	sum+=abs(aa[i*9+2] -v.vector()->getitem(i*3+2));
+	}
+	std::cout<<sum<<std::endl;
+*/
 }
